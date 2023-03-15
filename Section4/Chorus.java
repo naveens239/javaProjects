6301db776f161f60a78b402b0d30786895766f46
naveens239 <naveens239@gmail.com>
2023-03-15 11:22:13 +0530

    initial commit

--- Section4/Chorus.java
+++ Section4/Chorus.java
@@ -1,5 +1,17 @@
 public class Chorus{
     public static void main(String[] args) {
-        
+        //To call it many times you can repeat function calls.
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+
+    }
+    public static void singChorus(){
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
     }
 }
