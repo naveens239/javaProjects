342b144cdbc241c13f7a5a420808966018f5d33b
naveens239 <naveens239@gmail.com>
2023-04-25 13:54:19 +0530

    weather function update

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.3/Weather.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.3/Weather.java
@@ -5,7 +5,9 @@ public class Weather {
         double midnight = 55;      //temperature at midnight in fahrenheit
 
         //Task 3 - Call the printTemperatures function. 
-        
+        printTemperatures(noon);
+        printTemperatures(evening);
+        printTemperatures(midnight);
         
     }
 
@@ -16,11 +18,13 @@ public class Weather {
      * Function name: fahrenheitToCelsius - converts fahrenheit to celcius
      * @param fahrenheit (double)
      * @return celsius (double)
-     * 
-     * Inside the function:
      * 1. returns the temperature in celcius. C = (F - 32) * 5/9. 
      */
-
+    public static double fahrenheitToCelsius(double fahrenheit){
+        double celsius;
+        celsius = (fahrenheit-32) * 5/9;
+        return celsius;
+    }
      
     //Task 2: Make a function here. See the doc comments below. 
 
@@ -32,4 +36,8 @@ public class Weather {
      *  1. prints: F: <temperature in fahrenheit>.
      *  2. prints: C: <temperature in celsius> . 
      */
+    public static void printTemperatures(double fahrenheit){
+        System.out.println("F: "+fahrenheit);
+        System.out.println("C: "+fahrenheitToCelsius(fahrenheit));
+    }
 }
--- Section4/ReturnValues.java
+++ Section4/ReturnValues.java
@@ -1,14 +1,41 @@
 public class ReturnValues {
     public static void main(String[] args) {
             double area1  = calculateArea(2.3, 3.6);
-            System.out.println("Area: "+ area1);
-            double area2  = calculateArea(1.5, 3.5);
-            System.out.println("Area: "+ area2);
-            double area3  = calculateArea(2.5, 5.5);
-            System.out.println("Area: "+ area3);
+            printArea(2.3, 3.6, area1);
+            // System.out.println("Area: "+ area1);
+            // double area2  = calculateArea(1.5, 3.5);
+            // System.out.println("Area: "+ area2);
+            // double area3  = calculateArea(2.5, 5.5);
+            // System.out.println("Area: "+ area3);
+            // calculateArea(-2.3, 5);
+            
+
+            String englishExplanation = explainArea("English");
+            // String frenchExplanation = explainArea("French");
+            // String spanishExplanation = explainArea("Spanish");
+            // String italianExplanation = explainArea("Italian");
+            System.out.println(englishExplanation);
+
+
         }
         public static double calculateArea(double length, double width){
+            if (length<0 || width<0){
+                System.out.println("invalid Dimensions");
+                System.exit(0);
+            }
             double area = length * width;
             return area;
         } 
+
+        public static String explainArea(String language){
+            switch(language){
+                case "English": return "Area equals length * width";
+                case "French": return "La surface est egale a la longueur * la largeur"; 
+                case "Spanish": return "area es igual a largo * ancho";
+                default: return "Language not available";
+            }
+          }
+        public static void printArea(double length, double width, double area){
+            System.out.println("A rectangle with a length of "+length+ " and a width of "+ width+ " has an area of "+ area);
+        }
     }
