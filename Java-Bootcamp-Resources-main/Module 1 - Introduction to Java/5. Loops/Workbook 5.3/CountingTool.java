8ffbcf773fa19231cf5c2cd088e1bb0a82b127c3
naveens239 <naveens239@gmail.com>
2023-05-01 04:30:03 +0530

    Counting

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class CountingTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // See Learn the Part for the instructions.
-
+        // scans in the same line as it is print and not println
+        System.out.print("Hi Timmy! Choose a  number to count to: ");
+        int timNum = scan.nextInt();
+        System.out.println("Great! Here's how it's done");
+        for (int i=0; i<=timNum; i++){
+            System.out.print(i+" ");
+        }
         scan.close();
     }
 }
